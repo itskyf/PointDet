5b3da1f3861cc580488bb87c69d996e52dc86ec1
Ky Anh <40734986+itskyf@users.noreply.github.com>
2022-07-13 08:06:09 +0700

    refactor: ball query operator

--- src/pointdet/csrc/ball_query/ball_query.cu
+++ src/pointdet/csrc/ball_query/ball_query.cu
@@ -1,107 +1,65 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
 #include <ATen/ATen.h>
 #include <ATen/cuda/Exceptions.h>
 #include <c10/cuda/CUDAGuard.h>
 
-// A chunk of work is blocksize-many points of P1.
-// The number of potential chunks to do is N*(1+(P1-1)/blocksize)
-// call (1+(P1-1)/blocksize) chunks_per_cloud
-// These chunks are divided among the gridSize-many blocks.
-// In block b, we work on chunks b, b+gridSize, b+2*gridSize etc .
-// In chunk i, we work on cloud i/chunks_per_cloud on points starting from
-// blocksize*(i%chunks_per_cloud).
+#include "utils/cuda_utils.hpp"
 
-template <typename scalar_t>
-__global__ void BallQueryKernel(
-    const at::PackedTensorAccessor64<scalar_t, 3, at::RestrictPtrTraits> p1,
-    const at::PackedTensorAccessor64<scalar_t, 3, at::RestrictPtrTraits> p2,
-    const at::PackedTensorAccessor32<int32_t, 1, at::RestrictPtrTraits> lengths1,
-    const at::PackedTensorAccessor32<int32_t, 1, at::RestrictPtrTraits> lengths2,
-    at::PackedTensorAccessor32<int32_t, 3, at::RestrictPtrTraits> idxs, const int K,
-    const float radius2) {
-  const int N = p1.size(0);
-  const int chunks_per_cloud = (1 + (p1.size(1) - 1) / blockDim.x);
-  const int chunks_to_do = N * chunks_per_cloud;
-  const int D = p1.size(2);
-
-  for (int chunk = blockIdx.x; chunk < chunks_to_do; chunk += gridDim.x) {
-    const int n = chunk / chunks_per_cloud;  // batch_index
-    const int start_point = blockDim.x * (chunk % chunks_per_cloud);
-    int i = start_point + threadIdx.x;
-
-    // Check if point is valid in heterogeneous tensor
-    if (i >= lengths1[n]) {
-      continue;
+template <typename T>
+__global__ void ball_query_kernel(const T* centroids, const T* points, int* indices,
+                                  const int batch_size, const int num_groups,
+                                  const int num_neighbors, const int total_points,
+                                  const float radius2) {
+  const int bs_idx = blockIdx.y;
+  for (int pt_idx = blockIdx.x * blockDim.x + threadIdx.x; pt_idx < num_groups;
+       pt_idx += blockDim.x * gridDim.x) {
+    if (bs_idx >= batch_size) {
+      return;
     }
 
-    // Iterate over points in p2 until desired count is reached or
-    // all points have been considered
-    for (int j = 0, count = 0; j < lengths2[n] && count < K; ++j) {
-      // Calculate the distance between the points
-      scalar_t dist2 = 0.0;
-      for (int d = 0; d < D; ++d) {
-        scalar_t diff = p1[n][i][d] - p2[n][j][d];
-        dist2 += (diff * diff);
-      }
+    centroids += bs_idx * num_groups * 3 + pt_idx * 3;
+    points += bs_idx * total_points * 3;
+    indices += bs_idx * num_groups * num_neighbors + pt_idx * num_neighbors;
 
-      if (dist2 < radius2) {
-        // If the point is within the radius
-        // Set the value of the index to the point index
-        idxs[n][i][count] = j;
-        // increment the number of selected samples for the point i
-        ++count;
+    const T c_x = centroids[0], c_y = centroids[1], c_z = centroids[2];
+
+    int cnt = 0;
+    for (int k = 0; k < total_points; ++k) {
+      const T x = points[k * 3 + 0], y = points[k * 3 + 1], z = points[k * 3 + 2];
+      const T d2 = (c_x - x) * (c_x - x) + (c_y - y) * (c_y - y) + (c_z - z) * (c_z - z);
+      if (d2 < radius2) {
+        if (cnt == 0) {
+          for (int l = 0; l < num_neighbors; ++l) {
+            indices[l] = k;
+          }
+        }
+        indices[cnt] = k;
+        if (++cnt >= num_neighbors) {
+          break;
+        }
       }
     }
   }
 }
-
-at::Tensor BallQueryCuda(const at::Tensor& p1,        // (N, P1, 3)
-                         const at::Tensor& p2,        // (N, P2, 3)
-                         const at::Tensor& lengths1,  // (N,)
-                         const at::Tensor& lengths2,  // (N,)
-                         int K, float radius) {
-  // Check inputs are on the same device
-  at::TensorArg p1_t{p1, "p1", 1}, p2_t{p2, "p2", 2};
-  at::TensorArg lengths1_t{lengths1, "lengths1", 3}, lengths2_t{lengths2, "lengths2", 4};
-  at::CheckedFrom c = "BallQueryCuda";
-  at::checkAllSameGPU(c, {p1_t, p2_t, lengths1_t, lengths2_t});
-  at::checkAllSameType(c, {p1_t, p2_t});
-  TORCH_CHECK(p1.size(0) == p2.size(0), "Point sets must have the same batch dimension");
-  TORCH_CHECK(p1.size(2) == p2.size(2), "Point sets must have the same last dimension");
-
-  // Set the device for the kernel launch based on the device of p1
-  at::cuda::CUDAGuard device_guard(p1.device());
+at::Tensor BallQueryCuda(const at::Tensor& centroids, const at::Tensor& points,
+                         const int batch_size, const int num_groups, const int num_neighbors,
+                         const int total_points, const float radius2) {
+  at::cuda::CUDAGuard device_guard(centroids.device());
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  const int N = p1.size(0);
-  const int P1 = p1.size(1);
-  const float radius2 = radius * radius;
+  // blockIdx.x(col), blockIdx.y(row)
+  dim3 blocks(GetBlockSize(num_groups), batch_size);
+  dim3 threads(kThreadsPerBlock);
 
-  // Output tensor with indices of neighbors for each point in p1
-  auto int_dtype = p1.options().dtype(at::kInt);
-  auto idxs = at::full({N, P1, K}, -1, int_dtype);
+  auto int_opts = points.options().dtype(at::kInt);
+  at::Tensor indices = at::zeros({batch_size, num_groups, num_neighbors}, int_opts);
 
-  if (idxs.numel() == 0) {
-    AT_CUDA_CHECK(cudaGetLastError());
-    return idxs;
-  }
-
-  constexpr size_t blocks = 256, threads = 256;
-
-  AT_DISPATCH_FLOATING_TYPES(p1.scalar_type(), "ball_query_kernel_cuda", [&] {
-    BallQueryKernel<<<blocks, threads, 0, stream>>>(
-        p1.packed_accessor64<float, 3, at::RestrictPtrTraits>(),
-        p2.packed_accessor64<float, 3, at::RestrictPtrTraits>(),
-        lengths1.packed_accessor32<int32_t, 1, at::RestrictPtrTraits>(),
-        lengths2.packed_accessor32<int32_t, 1, at::RestrictPtrTraits>(),
-        idxs.packed_accessor32<int32_t, 3, at::RestrictPtrTraits>(), K, radius2);
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(centroids.scalar_type(), "ball_query_kernel", [&] {
+    ball_query_kernel<scalar_t><<<blocks, threads, 0, stream>>>(
+        centroids.data_ptr<scalar_t>(), points.data_ptr<scalar_t>(), indices.data_ptr<int>(),
+        batch_size, num_groups, num_neighbors, total_points, radius2);
   });
+
   AT_CUDA_CHECK(cudaGetLastError());
-  return idxs;
+
+  return indices;
 }
--- src/pointdet/ops/ball_query.py
+++ src/pointdet/ops/ball_query.py
@@ -1,102 +1,30 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-from typing import Optional
-
-import torch
+from torch import Tensor
 from torch.autograd import Function
-from torch.autograd.function import once_differentiable
+from torch.autograd.function import FunctionCtx, once_differentiable
 
 from pointdet import _C
 
 
-class _ball_query(Function):
+class BallQuery(Function):
     """
     Torch autograd Function wrapper for Ball Query C++/CUDA implementations.
     """
 
     @staticmethod
     def forward(
-        ctx,
-        centroids: torch.Tensor,
-        points: torch.Tensor,
-        num_neighbors: int,
-        radius: float,
-        lengths1: torch.Tensor,
-        lengths2: torch.Tensor,
+        ctx: FunctionCtx, centroids: Tensor, points: Tensor, num_neighbors: int, radius: float
     ):
         """
         Arguments defintions the same as in the ball_query function
         """
-        indices = _C.ball_query(centroids, points, lengths1, lengths2, num_neighbors, radius)
+        indices = _C.ball_query(centroids, points, num_neighbors, radius)
         ctx.mark_non_differentiable(indices)
         return indices
 
     @staticmethod
     @once_differentiable
-    def backward(ctx, grad_idx):
+    def backward(ctx: FunctionCtx, grad_indices: Tensor):
         return None, None, None, None
 
 
-def ball_query(
-    centroids: torch.Tensor,
-    points: torch.Tensor,
-    num_neighbors: int,
-    radius: float,
-    lengths1: Optional[torch.Tensor] = None,
-    lengths2: Optional[torch.Tensor] = None,
-):
-    """
-    Ball Query is an alternative to KNN. It can be
-    used to find all points in p2 that are within a specified radius
-    to the query point in p1 (with an upper limit of K neighbors).
-
-    The neighbors returned are not necssarily the *nearest* to the
-    point in p1, just the first K values in p2 which are within the
-    specified radius.
-
-    This method is faster than kNN when there are large numbers of points
-    in p2 and the ordering of neighbors is not important compared to the
-    distance being within the radius threshold.
-
-    "Ball query’s local neighborhood guarantees a fixed region scale thus
-    making local region features more generalizable across space, which is
-    preferred for tasks requiring local pattern recognition
-    (e.g. semantic point labeling)" [1].
-
-    [1] Charles R. Qi et al, "PointNet++: Deep Hierarchical Feature Learning
-        on Point Sets in a Metric Space", NeurIPS 2017.
-
-    Args:
-        centroids: Tensor of shape (B, K, D) giving a batch of B point clouds, each containing
-            up to P1 points of dimension D. These represent the centers of the ball queries.
-        points: Tensor of shape (B, N, D) giving a batch of B point clouds, each containing
-            up to P2 points of dimension D.
-        num_neighbors: upper bound on the number of samples to take within the radius
-        radius: the radius around each point within which the neighbors need to be located
-        lengths1: LongTensor of shape (B,) of values in the range [0, P1], giving the length
-            of each pointcloud in p1. Or None to indicate that every cloud has length P1.
-        lengths2: LongTensor of shape (B,) of values in the range [0, P2], giving the length
-            of each pointcloud in p2. Or None to indicate that every cloud has length P2.
-
-    Returns:
-        indices: (B, K, num_neighbors) the indices of S neighbors in p2 for points in p1.
-            Concretely, if `p1_idx[n, i, k] = j` then `p2[n, j]` is the k-th
-            neighbor to `p1[n, i]` in `p2[n]`. This is padded with -1 both where a cloud
-            in p2 has fewer than S points and where a cloud in p1 has fewer than P1 points
-            and also if there are fewer than K points which satisfy the radius threshold.
-    """
-    num_centers = centroids.size(1)
-    total_points = points.size(1)
-    batch_size = centroids.size(0)
-
-    device = centroids.device
-    if lengths1 is None:
-        lengths1 = torch.full((batch_size,), num_centers, dtype=torch.int, device=device)
-    if lengths2 is None:
-        lengths2 = torch.full((batch_size,), total_points, dtype=torch.int, device=device)
-
-    return _ball_query.apply(centroids, points, num_neighbors, radius, lengths1, lengths2)
+ball_query = BallQuery.apply

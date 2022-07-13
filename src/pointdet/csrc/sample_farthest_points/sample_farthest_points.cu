fcc2b4377aafd035eb13ba48a84222b4c53c5038
Ky Anh <40734986+itskyf@users.noreply.github.com>
2022-07-13 08:51:21 +0700

    feat: update farthest point sampling ops

--- src/pointdet/csrc/sample_farthest_points/sample_farthest_points.cu
+++ src/pointdet/csrc/sample_farthest_points/sample_farthest_points.cu
@@ -1,238 +1,143 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
 #include <ATen/ATen.h>
 #include <ATen/cuda/Exceptions.h>
 #include <c10/cuda/CUDAGuard.h>
 
-#include "utils/warp_reduce.cuh"
+#include <algorithm>
+
+__device__ void __update(float *__restrict__ dists, int *__restrict__ dists_i, const int idx1,
+                         const int idx2) {
+  const float v1 = dists[idx1], v2 = dists[idx2];
+  const int i1 = dists_i[idx1], i2 = dists_i[idx2];
+  dists[idx1] = std::max(v1, v2);
+  dists_i[idx1] = v2 > v1 ? i2 : i1;
+}
 
 template <unsigned int block_size>
-__global__ void FarthestPointSamplingKernel(
-    const at::PackedTensorAccessor64<float, 3, at::RestrictPtrTraits> points,
-    const at::PackedTensorAccessor64<int64_t, 1, at::RestrictPtrTraits> lengths,
-    const at::PackedTensorAccessor64<int64_t, 1, at::RestrictPtrTraits> K,
-    at::PackedTensorAccessor64<int64_t, 2, at::RestrictPtrTraits> idxs,
-    at::PackedTensorAccessor64<float, 2, at::RestrictPtrTraits> min_point_dist,
-    const at::PackedTensorAccessor64<int64_t, 1, at::RestrictPtrTraits> start_idxs) {
-  // Get constants
-  const int64_t N = points.size(0);
-  const int64_t P = points.size(1);
-  const int64_t D = points.size(2);
+__global__ void farthest_point_sampling_kernel(const float *__restrict__ points,
+                                               float *__restrict__ temp, int *__restrict__ indices,
+                                               int batch_size, int total_points, int num_points) {
+  if (num_points <= 0) {
+    return;
+  }
+  __shared__ float dists[block_size];
+  __shared__ int dists_i[block_size];
 
-  // Create single shared memory buffer which is split and cast to different
-  // types: dists/dists_idx are used to save the maximum distances seen by the
-  // points processed by any one thread and the associated point indices.
-  // These values only need to be accessed by other threads in this block which
-  // are processing the same batch and not by other blocks.
-  extern __shared__ char shared_buf[];
-  float* dists = (float*)shared_buf;                  // block_size floats
-  int64_t* dists_idx = (int64_t*)&dists[block_size];  // block_size int64_t
+  const int batch_idx = blockIdx.x;
+  points += batch_idx * total_points * 3;
+  temp += batch_idx * total_points;
+  indices += batch_idx * num_points;
 
-  // Get batch index and thread index
-  const int64_t batch_idx = blockIdx.x;
-  const size_t tid = threadIdx.x;
+  int tid = threadIdx.x;
+  const int stride = block_size;
 
-  // If K is greater than the number of points in the pointcloud
-  // we only need to iterate until the smaller value is reached.
-  const int64_t k_n = min(K[batch_idx], lengths[batch_idx]);
+  int old = 0;
+  if (threadIdx.x == 0) {
+    indices[0] = old;
+  }
 
-  // Write the first selected point to global memory in the first thread
-  int64_t selected = start_idxs[batch_idx];
-  if (tid == 0) idxs[batch_idx][0] = selected;
+  __syncthreads();
+  for (int j = 1; j < num_points; ++j) {
+    int besti = 0;
+    float best = -1;
+    float x1 = points[old * 3 + 0];
+    float y1 = points[old * 3 + 1];
+    float z1 = points[old * 3 + 2];
+    for (int k = tid; k < total_points; k += stride) {
+      float x2, y2, z2;
+      x2 = points[k * 3 + 0];
+      y2 = points[k * 3 + 1];
+      z2 = points[k * 3 + 2];
 
-  // Iterate to find k_n sampled points
-  for (int64_t k = 1; k < k_n; ++k) {
-    // Keep track of the maximum of the minimum distance to previously selected
-    // points seen by this thread
-    int64_t max_dist_idx = 0;
-    float max_dist = -1.0;
-
-    // Iterate through all the points in this pointcloud. For already selected
-    // points, the minimum distance to the set of previously selected points
-    // will be 0.0 so they won't be selected again.
-    for (int64_t p = tid; p < lengths[batch_idx]; p += block_size) {
-      // Calculate the distance to the last selected point
-      float dist2 = 0.0;
-      for (int64_t d = 0; d < D; ++d) {
-        float diff = points[batch_idx][selected][d] - points[batch_idx][p][d];
-        dist2 += (diff * diff);
-      }
-
-      // If the distance of point p to the last selected point is
-      // less than the previous minimum distance of p to the set of selected
-      // points, then updated the corresponding value in min_point_dist
-      // so it always contains the min distance.
-      const float p_min_dist = min(dist2, min_point_dist[batch_idx][p]);
-      min_point_dist[batch_idx][p] = p_min_dist;
-
-      // Update the max distance and point idx for this thread.
-      max_dist_idx = (p_min_dist > max_dist) ? p : max_dist_idx;
-      max_dist = (p_min_dist > max_dist) ? p_min_dist : max_dist;
+      float d = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1);
+      float d2 = fmin(d, temp[k]);
+      temp[k] = d2;
+      besti = d2 > best ? k : besti;
+      best = d2 > best ? d2 : best;
     }
-
-    // After going through all points for this thread, save the max
-    // point and idx seen by this thread. Each thread sees P/block_size points.
-    dists[tid] = max_dist;
-    dists_idx[tid] = max_dist_idx;
-    // Sync to ensure all threads in the block have updated their max point.
+    dists[tid] = best;
+    dists_i[tid] = besti;
     __syncthreads();
 
-    // Parallelized block reduction to find the max point seen by
-    // all the threads in this block for iteration k.
-    // Each block represents one batch element so we can use a divide/conquer
-    // approach to find the max, syncing all threads after each step.
-
-    for (int s = block_size / 2; s > 0; s >>= 1) {
-      if (tid < s) {
-        // Compare the best point seen by two threads and update the shared
-        // memory at the location of the first thread index with the max out
-        // of the two threads.
-        if (dists[tid] < dists[tid + s]) {
-          dists[tid] = dists[tid + s];
-          dists_idx[tid] = dists_idx[tid + s];
-        }
+#pragma unroll
+    for (int block_size_thres = 1024; block_size_thres >= 2; block_size_thres >>= 1) {
+      const int tid_thres = block_size_thres / 2;
+      if (block_size >= block_size_thres && tid < tid_thres) {
+        __update(dists, dists_i, tid, tid + tid_thres);
       }
       __syncthreads();
     }
 
-    // TODO(nikhilar): As reduction proceeds, the number of “active” threads
-    // decreases. When tid < 32, there should only be one warp left which could
-    // be unrolled.
-
-    // The overall max after reducing will be saved
-    // at the location of tid = 0.
-    selected = dists_idx[0];
-
+    old = dists_i[0];
     if (tid == 0) {
-      // Write the farthest point for iteration k to global memory
-      idxs[batch_idx][k] = selected;
+      indices[j] = old;
     }
   }
 }
 
-at::Tensor FarthestPointSamplingCuda(const at::Tensor& points,   // (N, P, 3)
-                                     const at::Tensor& lengths,  // (N,)
-                                     const at::Tensor& K,        // (N,)
-                                     const at::Tensor& start_idxs) {
-  // Check inputs are on the same device
-  at::TensorArg p_t{points, "points", 1}, lengths_t{lengths, "lengths", 2}, k_t{K, "K", 3},
-      start_idxs_t{start_idxs, "start_idxs", 4};
-  at::CheckedFrom c = "FarthestPointSamplingCuda";
-  at::checkAllSameGPU(c, {p_t, lengths_t, k_t, start_idxs_t});
-  at::checkAllSameType(c, {lengths_t, k_t, start_idxs_t});
-
-  // Set the device for the kernel launch based on the device of points
-  at::cuda::CUDAGuard device_guard(points.device());
+at::Tensor FarthestPointSamplingCuda(const at::Tensor &points, const int batch_size,
+                                     const int total_points, const int num_points) {
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  TORCH_CHECK(points.size(0) == lengths.size(0),
-              "Point and lengths must have the same batch dimension");
+  auto pt_ops = points.options().dtype(at::kInt);
+  at::Tensor indices = at::zeros({batch_size, num_points}, pt_ops.dtype(at::kInt));
+  at::Tensor tmp_tensor = at::full({batch_size, total_points}, 1e10, pt_ops);
 
-  TORCH_CHECK(points.size(0) == K.size(0), "Points and K must have the same batch dimension");
+  const float *p_points = points.data_ptr<float>();
+  float *p_tmp = tmp_tensor.data_ptr<float>();
+  int *p_indices = indices.data_ptr<int>();
 
-  const int64_t N = points.size(0);
-  const int64_t P = points.size(1);
-  const int64_t max_K = at::max(K).item<int64_t>();
-
-  // Initialize the output tensor with the sampled indices
-  auto idxs = at::full({N, max_K}, -1, lengths.options());
-  auto min_point_dist = at::full({N, P}, 1e10, points.options());
-
-  if (N == 0 || P == 0) {
-    AT_CUDA_CHECK(cudaGetLastError());
-    return idxs;
-  }
-
-  // Set the number of blocks to the batch size so that the
-  // block reduction step can be done for each pointcloud
-  // to find the max distance point in the pointcloud at each iteration.
-  const size_t blocks = N;
-
-  // Set the threads to the nearest power of 2 of the number of
-  // points in the pointcloud (up to the max threads in a block).
-  // This will ensure each thread processes the minimum necessary number of
-  // points (P/threads).
-  const int points_pow_2 = std::log(static_cast<double>(P)) / std::log(2.0);
-
-  // Max possible threads per block
-  const int MAX_THREADS_PER_BLOCK = 1024;
-  const size_t threads = max(min(1 << points_pow_2, MAX_THREADS_PER_BLOCK), 1);
-
-  // Create the accessors
-  auto points_a = points.packed_accessor64<float, 3, at::RestrictPtrTraits>();
-  auto lengths_a = lengths.packed_accessor64<int64_t, 1, at::RestrictPtrTraits>();
-  auto K_a = K.packed_accessor64<int64_t, 1, at::RestrictPtrTraits>();
-  auto idxs_a = idxs.packed_accessor64<int64_t, 2, at::RestrictPtrTraits>();
-  auto start_idxs_a = start_idxs.packed_accessor64<int64_t, 1, at::RestrictPtrTraits>();
-  auto min_point_dist_a = min_point_dist.packed_accessor64<float, 2, at::RestrictPtrTraits>();
-
-  // Initialize the shared memory which will be used to store the
-  // distance/index of the best point seen by each thread.
-  size_t shared_mem = threads * sizeof(float) + threads * sizeof(int64_t);
-  // TODO: using shared memory for min_point_dist gives an ~2x speed up
-  // compared to using a global (N, P) shaped tensor, however for
-  // larger pointclouds this may exceed the shared memory limit per block.
-  // If a speed up is required for smaller pointclouds, then the storage
-  // could be switched to shared memory if the required total shared memory is
-  // within the memory limit per block.
-
-  // Support a case for all powers of 2 up to MAX_THREADS_PER_BLOCK possible per
-  // block.
-  switch (threads) {
+  const int pow_2 = std::log(static_cast<double>(total_points)) / std::log(2.0);
+  const int n_threads = std::max(std::min(1 << pow_2, 1024), 1);
+  switch (n_threads) {
     case 1024:
-      FarthestPointSamplingKernel<1024><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<1024><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 512:
-      FarthestPointSamplingKernel<512><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<512><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 256:
-      FarthestPointSamplingKernel<256><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<256><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 128:
-      FarthestPointSamplingKernel<128><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<128><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 64:
-      FarthestPointSamplingKernel<64><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<64><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 32:
-      FarthestPointSamplingKernel<32><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<32><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 16:
-      FarthestPointSamplingKernel<16><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<16><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 8:
-      FarthestPointSamplingKernel<8><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<8><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 4:
-      FarthestPointSamplingKernel<4><<<threads, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<4><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 2:
-      FarthestPointSamplingKernel<2><<<threads, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<2><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     case 1:
-      FarthestPointSamplingKernel<1><<<threads, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<1><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
       break;
     default:
-      FarthestPointSamplingKernel<1024><<<blocks, threads, shared_mem, stream>>>(
-          points_a, lengths_a, K_a, idxs_a, min_point_dist_a, start_idxs_a);
+      farthest_point_sampling_kernel<512><<<batch_size, n_threads, 0, stream>>>(
+          p_points, p_tmp, p_indices, batch_size, total_points, num_points);
   }
 
   AT_CUDA_CHECK(cudaGetLastError());
-  return idxs;
+  return indices;
 }
--- src/pointdet/ops/sampling.py
+++ src/pointdet/ops/sampling.py
@@ -1,13 +1,6 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-from random import randint
-from typing import Optional, Union
-
 import torch
+from torch.autograd import Function
+from torch.autograd.function import FunctionCtx
 
 from pointdet import _C
 
@@ -25,148 +18,27 @@ def centroid_aware(cls_features: torch.Tensor, num_points: int) -> torch.Tensor:
     return out.indices
 
 
-def sample_farthest_points(
-    points: torch.Tensor,
-    num_points: Union[int, list[int], torch.Tensor] = 50,
-    lengths: Optional[torch.Tensor] = None,
-    random_start: bool = False,
-) -> torch.Tensor:
-    """
-    Iterative farthest point sampling algorithm [1] to subsample a set of
-    K points from a given pointcloud. At each iteration, a point is selected
-    which has the largest nearest neighbor distance to any of the
-    already selected points.
+class FurthestPointSampling(Function):
+    """Uses iterative furthest point sampling to select a set of features whose
+    corresponding points have the furthest distance."""
 
-    Farthest point sampling provides more uniform coverage of the input
-    point cloud compared to uniform random sampling.
+    @staticmethod
+    def forward(ctx: FunctionCtx, points: torch.Tensor, num_points: int):
+        """
+        Args:
+            points_xyz (torch.Tensor): (B, N, 3) where N > num_points.
+            num_points (int): Number of points in the sampled set.
 
-    [1] Charles R. Qi et al, "PointNet++: Deep Hierarchical Feature Learning
-        on Point Sets in a Metric Space", NeurIPS 2017.
+        Returns:
+            torch.Tensor: (B, num_points) indices of the sampled points.
+        """
+        indices = _C.sample_farthest_points(points, num_points)
+        ctx.mark_non_differentiable(indices)
+        return indices
 
-    Args:
-        points: (B, P, D) array containing the batch of pointclouds
-        lengths: (B,) number of points in each pointcloud (to support heterogeneous
-            batches of pointclouds)
-        num_points: samples required in each sampled point cloud (this is typically << P). If
-            K is an int then the same number of samples are selected for each
-            pointcloud in the batch. If K is a tensor is should be length (N,)
-            giving the number of samples to select for each element in the batch
-        random_start: bool, if True, a random point is selected as the starting point.
-
-    Returns:
-        indices: (B, K) array of selected indices. If the input K is a tensor, then the shape
-            will be (B, max(K), D), and padded with -1 for batch elements where k_i < max(K).
-    """
-    batch_size, total_points = points.shape[:2]
-    device = points.device
-    # Validate inputs
-    if lengths is None:
-        lengths = torch.full((batch_size,), total_points, dtype=torch.int64, device=device)
-    # TODO: support providing K as a ratio of the total number of points instead of as an int
-    if isinstance(num_points, int):
-        num_points = torch.full((batch_size,), num_points, dtype=torch.int64, device=device)
-    elif isinstance(num_points, list):
-        num_points = torch.tensor(num_points, dtype=torch.int64, device=device)
-
-    # Check dtypes are correct and convert if necessary
-    if points.dtype != torch.float32:
-        points = points.float()
-    if lengths.dtype != torch.int64:
-        lengths = lengths.long()
-    if num_points.dtype != torch.int64:
-        num_points = num_points.long()
-
-    # Generate the starting indices for sampling
-    start_idxs = torch.zeros_like(lengths)
-    if random_start:
-        for b_idx in range(batch_size):
-            # pyre-fixme[6]: For 1st param expected `int` but got `Tensor`.
-            start_idxs[b_idx] = torch.randint(high=lengths[b_idx], size=(1,)).item()
-
-    with torch.no_grad():
-        indices = _C.sample_farthest_points(points, lengths, num_points, start_idxs)
-    return indices
+    @staticmethod
+    def backward(ctx, grad_indices):
+        return None, None
 
 
-def sample_farthest_points_naive(
-    points: torch.Tensor,
-    num_points: Union[int, list[int], torch.Tensor] = 50,
-    lengths: Optional[torch.Tensor] = None,
-    random_start: bool = False,
-) -> torch.Tensor:
-    """
-    Same Args/Returns as sample_farthest_points
-    """
-    batch_size, total_points = points.shape[:2]
-    device = points.device
-
-    # Validate inputs
-    if lengths is None:
-        lengths = torch.full((batch_size,), total_points, dtype=torch.int64, device=device)
-
-    if lengths.shape[0] != batch_size:
-        raise ValueError("points and lengths must have same batch dimension.")
-
-    # TODO: support providing K as a ratio of the total number of points instead of as an int
-    if isinstance(num_points, int):
-        num_points = torch.full((batch_size,), num_points, dtype=torch.int64, device=device)
-    elif isinstance(num_points, list):
-        num_points = torch.tensor(num_points, dtype=torch.int64, device=device)
-    if num_points.size(0) != batch_size:
-        raise ValueError("K and points must have the same batch dimension")
-
-    # Find max value of K
-    max_k = torch.max(num_points)
-    # List of selected indices from each batch element
-    all_sampled_indices = []
-
-    for n in range(batch_size):
-        # Initialize an array for the sampled indices, shape: (max_K,)
-        # pyre-fixme[6]: For 1st param expected `Union[List[int], Size,
-        #  typing.Tuple[int, ...]]` but got `Tuple[Tensor]`.
-        sample_idx_batch = torch.full((max_k,), fill_value=-1, dtype=torch.int64, device=device)
-
-        # Initialize closest distances to inf, shape: (P,)
-        # This will be updated at each iteration to track the closest distance of the
-        # remaining points to any of the selected points
-        # pyre-fixme[6]: For 1st param expected `Union[List[int], Size,
-        #  typing.Tuple[int, ...]]` but got `Tuple[Tensor]`.
-        closest_dists = points.new_full((lengths[n],), float("inf"), dtype=torch.float32)
-
-        # Select a random point index and save it as the starting point
-        selected_idx = randint(0, lengths[n] - 1) if random_start else 0
-        sample_idx_batch[0] = selected_idx
-
-        # If the pointcloud has fewer than K points then only iterate over the min
-        # pyre-fixme[6]: For 1st param expected `SupportsRichComparisonT` but got
-        #  `Tensor`.
-        # pyre-fixme[6]: For 2nd param expected `SupportsRichComparisonT` but got
-        #  `Tensor`.
-        k_n = min(lengths[n], num_points[n])
-
-        # Iteratively select points for a maximum of k_n
-        for i in range(1, k_n):
-            # Find the distance between the last selected point
-            # and all the other points. If a point has already been selected
-            # it's distance will be 0.0 so it will not be selected again as the max.
-            dist = points[n, selected_idx, :] - points[n, : lengths[n], :]
-            # pyre-fixme[58]: `**` is not supported for operand types `Tensor` and
-            #  `int`.
-            dist_to_last_selected = (dist**2).sum(-1)  # (P - i)
-
-            # If closer than currently saved distance to one of the selected
-            # points, then updated closest_dists
-            closest_dists = torch.min(dist_to_last_selected, closest_dists)  # (P - i)
-
-            # The aim is to pick the point that has the largest
-            # nearest neighbour distance to any of the already selected points
-            selected_idx = torch.argmax(closest_dists)
-            sample_idx_batch[i] = selected_idx
-
-        # Add the list of points for this batch to the final list
-        all_sampled_indices.append(sample_idx_batch)
-
-    all_sampled_indices = torch.stack(all_sampled_indices)
-    # Gather the points
-    # Return (N, max_K, D) subsampled points and indices
-    return all_sampled_indices
+sample_farthest_points = FurthestPointSampling.apply
